19f7b407911e468c7a352f78e0e64263667e6b83
cogdotnet1685 <cogdotnet1685@CTSDOTNET803>
2022-08-30 17:54:46 +0530

    Add project files.

---
+++ DatabaseFirstApproachWebAPI/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DatabaseFirstApproachWebAPI.Models
+{
+    public partial class Employee
+    {
+        public int EmpIdId { get; set; }
+        public string EmpName { get; set; } = null!;
+        public int DepartmentId { get; set; }
+
+        public virtual Department Department { get; set; } = null!;
+    }
+}
---
+++ DatabaseFirstApproachWebAPI/Models/WebAPIDatabaseFirstApproachContext.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DatabaseFirstApproachWebAPI.Models
+{
+    public partial class WebAPIDatabaseFirstApproachContext : DbContext
+    {
+        public WebAPIDatabaseFirstApproachContext()
+        {
+        }
+
+        public WebAPIDatabaseFirstApproachContext(DbContextOptions<WebAPIDatabaseFirstApproachContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Department> Departments { get; set; } = null!;
+        public virtual DbSet<Employee> Employees { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=WebAPIDatabaseFirstApproach;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Department>(entity =>
+            {
+                entity.ToTable("Department");
+
+                entity.Property(e => e.DepartmentId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("DepartmentID");
+
+                entity.Property(e => e.DepartmentName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.HasKey(e => e.EmpIdId)
+                    .HasName("PK__Employee__035F6C27CFB746B5");
+
+                entity.ToTable("Employee");
+
+                entity.Property(e => e.EmpIdId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("EmpIdID");
+
+                entity.Property(e => e.EmpName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.Department)
+                    .WithMany(p => p.Employees)
+                    .HasForeignKey(d => d.DepartmentId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Employee__Depart__440B1D61");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
